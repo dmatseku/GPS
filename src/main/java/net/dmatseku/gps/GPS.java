fa4407b8c966a8b7f23fae0559deddfb5e3ca347
nloiko <nloiko@barcodesinc.com>
2021-03-04 00:11:46 +0200

    first version of the mod

---
+++ src/main/java/net/dmatseku/gps/GPS.java
@@ -0,0 +1,23 @@
+package net.dmatseku.gps;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+import static com.mojang.brigadier.arguments.StringArgumentType.word;
+import static net.minecraft.server.command.CommandManager.literal;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.*;
+
+public class GPS implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> {
+			dispatcher.register(literal("gps").executes(context -> {
+				System.out.println("hello");
+				return 1;
+			}));
+		}));
+	}
+}
